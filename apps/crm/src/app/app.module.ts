fa3049902402f415a645113fe7936aaa59ba9faa
misha28x <misha1998x@gmail.com>
2020-08-17 20:54:22 +0300

    added ng-zorro

--- apps/crm/src/app/app.module.ts
+++ apps/crm/src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { uk_UA } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import uk from '@angular/common/locales/uk';
+
+registerLocaleData(uk);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [{ provide: NZ_I18N, useValue: uk_UA }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
