3ac9d68581dfaec995b3f55318de9348213bfa1f
misha28x <misha1998x@gmail.com>
2021-03-10 14:12:48 +0200

    migrated nx to latest

--- libs/crm/layout/src/lib/default-layout/default-layout.component.spec.ts
+++ libs/crm/layout/src/lib/default-layout/default-layout.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DefaultLayoutComponent } from './default-layout.component';
 
@@ -6,7 +6,7 @@ describe('DefaultComponent', () => {
   let component: DefaultLayoutComponent;
   let fixture: ComponentFixture<DefaultLayoutComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DefaultLayoutComponent ]
     })
